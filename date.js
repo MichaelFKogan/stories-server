67b0bc71384fe063ec164317874e4af72744bc21
Michael Kogan <mfkogan@gmail.com>
2025-01-22 07:03:04 -0500

    added new server endpoint /date

---
+++ date.js
@@ -0,0 +1,25 @@
+// stories.js
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// const top = require('./stories/top');
+// const storiesOne = require('./stories/storiesOne');
+// const storiesThree = require('./stories/storiesThree');
+const storiesTwo = require('./stories/storiesTwo');
+
+const stories = [
+  // ...top,
+  // ...storiesOne,
+  // ...storiesThree,
+  ...storiesTwo,
+];
+
+app.get('/date', (req, res) => {
+  res.json(stories);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
