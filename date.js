04f30e5a2b07884c1d0c875047f88efcbb2910e4
Michael Kogan <mfkogan@gmail.com>
2025-01-22 22:43:43 -0500

    updates

--- date.js
+++ date.js
@@ -5,12 +5,19 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // const top = require('./stories/top');
-// const storiesOne = require('./stories/storiesOne');
+const storiesOne = require('./stories/storiesOne');
 // const storiesThree = require('./stories/storiesThree');
 const storiesTwo = require('./stories/storiesTwo');
 
 const data = {
+
   date: "Jan, 22 2025",
+  stories: [
+    ...storiesOne,
+  ],
+
+
+  date: "Jan, 21 2025",
   stories: [
     ...storiesTwo,
   ],
